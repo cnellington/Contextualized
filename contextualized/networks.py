7e9d3ed4ac382cb1c2ba15a95bd58d8d82cfda72
Caleb Ellington <caleb.n.ellington@gmail.com>
2022-03-07 12:05:03 -0500

    added dummy training loop to main as a quick sanity check

--- contextualized/networks.py
+++ contextualized/networks.py
@@ -6,7 +6,7 @@ from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 
 from contextualized.modules import NGAM, MLP, SoftSelect, Explainer
-from contextualized.regression import CRTrainer, TasksplitContextualizedUnivariateRegression
+from contextualized.regression import RegressionTrainer, TasksplitContextualizedUnivariateRegression
 
 
 ENCODERS = {
@@ -20,7 +20,7 @@ LINK_FNS = [
 ]
 
 
-class CorrTrainer(CRTrainer):
+class NetworkTrainer(RegressionTrainer):
     def predict_network(self, model, dataloader):
         betas, _ = super().predict_params(model, dataloader)
         rhos = betas * np.transpose(betas, (0, 2, 1))
@@ -33,7 +33,7 @@ class ContextualizedCorrelation(TasksplitContextualizedUnivariateRegression):
     
     def dataloader(self, C, X, **kwargs):
         return super().dataloader(C, X, X, **kwargs)
-   
+
 
 if __name__ == '__main__':
     n = 100
@@ -52,13 +52,22 @@ if __name__ == '__main__':
     Y_2 = X[:, 1].unsqueeze(-1) * W_2 + b_2
     Y_3 = X.sum(axis=1).unsqueeze(-1)
     Y = torch.cat((Y_1, Y_2, Y_3), axis=1)
-    
+
+    k = 10
+    epochs = 2
+    batch_size = 1
+    C, X, Y = C.numpy(), X.numpy(), Y.numpy()
+
+    def quicktest(model):
+        dataloader = model.dataloader(C, X, batch_size=32)
+        trainer = NetworkTrainer(max_epochs=1)
+        trainer.fit(model, dataloader)
+        trainer.validate(model, dataloader)
+        trainer.test(model, dataloader)
+        beta_preds, mu_preds = trainer.predict_params(model, dataloader)
+        y_preds = trainer.predict_y(model, dataloader)
+        network_preds = trainer.predict_network(model, dataloader)
+
+    # Correlation
     model = ContextualizedCorrelation(c_dim, x_dim)
-    dataloader = model.dataloader(C, X, batch_size=32)
-    trainer = CorrTrainer(max_epochs=10)
-    trainer.fit(model, dataloader)
-    beta_preds, mu_preds = trainer.predict_params(model, dataloader)
-    y_preds = trainer.predict_y(model, dataloader)
-    rhos = trainer.predict_network(model, dataloader)
-    trainer.test(model, dataloader)
-    # reg.reshape_preds(preds, dataloader)[0].shape
+    quicktest(model)
--- contextualized/regression.py
+++ contextualized/regression.py
@@ -22,7 +22,7 @@ LINK_FNS = [
 ]
 
 
-class CRTrainer(pl.Trainer):
+class RegressionTrainer(pl.Trainer):
     def predict_params(self, model, dataloader):
         preds = super().predict(model, dataloader)
         return model._params_reshape(preds, dataloader)
@@ -521,3 +521,57 @@ class TasksplitContextualizedUnivariateRegression(ContextualizedRegressionBase):
     
     def dataloader(self, C, X, Y, batch_size=32):
         return DataLoader(dataset=DataIterable(MultitaskUnivariateDataset(C, X, Y)), batch_size=batch_size)
+
+
+if __name__ == '__main__':
+    n = 100
+    c_dim = 4
+    x_dim = 2
+    y_dim = 3
+    C = torch.rand((n, c_dim)) - .5 
+    W_1 = C.sum(axis=1).unsqueeze(-1) ** 2
+    W_2 = - C.sum(axis=1).unsqueeze(-1)
+    b_1 = C[:, 0].unsqueeze(-1)
+    b_2 = C[:, 1].unsqueeze(-1)
+    W_full = torch.cat((W_1, W_2), axis=1)
+    b_full = b_1 + b_2
+    X = torch.rand((n, x_dim)) - .5
+    Y_1 = X[:, 0].unsqueeze(-1) * W_1 + b_1
+    Y_2 = X[:, 1].unsqueeze(-1) * W_2 + b_2
+    Y_3 = X.sum(axis=1).unsqueeze(-1)
+    Y = torch.cat((Y_1, Y_2, Y_3), axis=1)
+
+    k = 10
+    epochs = 2
+    batch_size = 1
+    C, X, Y = C.numpy(), X.numpy(), Y.numpy()
+
+    def quicktest(model):
+        dataloader = model.dataloader(C, X, Y, batch_size=32)
+        trainer = RegressionTrainer(max_epochs=1)
+        trainer.fit(model, dataloader)
+        trainer.validate(model, dataloader)
+        trainer.test(model, dataloader)
+        beta_preds, mu_preds = trainer.predict_params(model, dataloader)
+        y_preds = trainer.predict_y(model, dataloader)
+
+
+    # Naive Multivariate
+    model = NaiveContextualizedRegression(c_dim, x_dim, y_dim)
+    quicktest(model)
+
+    # Subtype Multivariate
+    model = SubtypeContextualizedRegression(c_dim, x_dim, y_dim)
+    quicktest(model)
+
+    # Multitask Multivariate
+    model = MultitaskContextualizedRegression(c_dim, x_dim, y_dim)
+    quicktest(model)
+
+    # Tasksplit Multivariate
+    model = TasksplitContextualizedRegression(c_dim, x_dim, y_dim)
+    quicktest(model)
+
+    # Tasksplit Univariate
+    model = TasksplitContextualizedRegression(c_dim, x_dim, y_dim)
+    quicktest(model)
