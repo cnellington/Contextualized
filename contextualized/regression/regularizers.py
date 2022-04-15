b69c26db1bc3c0285a499d6740c9a10af80e0c87
Ben Lengerich <blengerich@gmail.com>
2022-04-15 15:16:54 -0400

    fix l1l2 regularizer

--- contextualized/regression/regularizers.py
+++ contextualized/regression/regularizers.py
@@ -16,4 +16,6 @@ def l2_reg(alpha, mu_ratio=0.5):
 
 
 def l1_l2_reg(alpha, l1_ratio=0.5, mu_ratio=0.5):
-    return l1_ratio*l1_reg(alpha, mu_ratio) + (1-l1_ratio)*l2_reg(alpha, mu_ratio)
+    return lambda beta, mu: alpha*(
+        l1_ratio*(mu_ratio*torch.norm(mu, p=1) + (1-mu_ratio)*torch.norm(beta, p=1)) +
+        (1-l1_ratio)*(mu_ratio*torch.norm(mu, p=2) + (1-mu_ratio)*torch.norm(beta, p=2))).mean()
