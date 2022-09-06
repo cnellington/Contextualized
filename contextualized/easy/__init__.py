a1116032c550ea515c29d8113fdc0dbe7cb6296c
Ben Lengerich <blengerich@gmail.com>
2022-09-05 23:05:07 -0400

    add ContextualGAM and demos

--- contextualized/easy/__init__.py
+++ contextualized/easy/__init__.py
@@ -1,2 +1,3 @@
 from contextualized.easy.ContextualizedRegressor import ContextualizedRegressor
 from contextualized.easy.ContextualizedClassifier import ContextualizedClassifier
+from contextualized.easy.ContextualGAM import ContextualGAMClassifier, ContextualGAMRegressor
