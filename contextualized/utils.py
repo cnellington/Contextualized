124c2968703aecd6069f2af976b6bac7cf956e82
Ben Lengerich <blengerich@gmail.com>
2022-10-20 16:26:29 -0400

    add docstrings to utils

--- contextualized/utils.py
+++ contextualized/utils.py
@@ -1,11 +1,26 @@
+"""
+Utility functions, including saving/loading of contextualized models.
+"""
+
 import torch
 import dill
 
 
 def save(model, path):
+    """
+    Saves model to path.
+    :param model: 
+    :param path: 
+
+    """
     torch.save(model, open(path, "wb"), pickle_module=dill)
 
 
 def load(path):
+    """
+    Loads model from path.
+    :param path: 
+
+    """
     model = torch.load(open(path, "rb"), pickle_module=dill)
     return model
