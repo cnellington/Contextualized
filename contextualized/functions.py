2a8993acfec19398f96c1e75268f30ea06ca79ef
Ben Lengerich <blengerich@gmail.com>
2022-10-20 16:16:23 -0400

    add docstrings to contextualized and functions

--- contextualized/functions.py
+++ contextualized/functions.py
@@ -1,3 +1,7 @@
+"""Utility torch mathematical functions which are 
+used for many modules.
+"""
+
 import torch
 import torch.nn.functional as F
 
@@ -12,26 +16,59 @@ softmax_link = lambda x, m, b: F.softmax(x * m + b, dim=1)
 
 
 def make_fn(base_fn, **params):
+    """
+    Makes a single-parameter function from a base function class and a fixed set of extra parameters.
+    :param base_fn:
+    :param **params:
+
+    """
     return lambda x: base_fn(x, **params)
 
 
 def linear_constructor(m=1, b=0):
+    """
+    Creates a single-parameter linear function with slope m and offset b.
+    :param m:  (Default value = 1)
+    :param b:  (Default value = 0)
+
+    """
     return make_fn(linear, m=m, b=b)
 
 
 def logistic_constructor(m=1, b=0):
+    """
+    Creates a single-parameter logistic function with slope m and offset b.
+    :param m:  (Default value = 1)
+    :param b:  (Default value = 0)
+
+    """
     return make_fn(logistic, m=m, b=b)
 
 
 def identity_link_constructor():
+    """
+    Creates a single-parameter identity function.
+    """
     return make_fn(identity_link)
 
 
 def linear_link_constructor(m=1, b=0):
+    """
+    Creates a single-parameter linear link function with slope m and offset b.
+    :param m:  (Default value = 1)
+    :param b:  (Default value = 0)
+
+    """
     return make_fn(linear_link, m=m, b=b)
 
 
 def softmax_link_constructor(m=1, b=0):
+    """
+    Creates a single-parameter softmax link function with slope m and offset b.
+    :param m:  (Default value = 1)
+    :param b:  (Default value = 0)
+
+    """
     return make_fn(softmax_link, m=m, b=b)
 
 
