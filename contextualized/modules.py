4262f2576c4f14b52611a6df716929bee4ea0d8e
Ben Lengerich <blengerich@gmail.com>
2022-10-20 20:38:06 -0400

    lint modules

--- contextualized/modules.py
+++ contextualized/modules.py
@@ -1,7 +1,10 @@
+"""
+PyTorch modules which are used as building blocks of Contextualized models.
+"""
 import torch
-import torch.nn as nn
+from torch import nn
 
-from contextualized.functions import identity_link, identity
+from contextualized.functions import identity_link
 
 
 class SoftSelect(nn.Module):
@@ -11,29 +14,31 @@ class SoftSelect(nn.Module):
     """
 
     def __init__(self, in_dims, out_shape):
-        super(SoftSelect, self).__init__()
+        super().__init__()
         self.in_dims = in_dims
         self.out_shape = out_shape
         init_mat = torch.rand(list(out_shape) + list(in_dims)) * 2e-2 - 1e-2
         self.archetypes = nn.parameter.Parameter(init_mat, requires_grad=True)
 
     def forward(self, *batch_weights):
+        """ Torch Forward pass.
+        """
         batch_size = batch_weights[0].shape[0]
         expand_dims = [batch_size] + [-1 for _ in range(len(self.archetypes.shape))]
         batch_archetypes = self.archetypes.unsqueeze(0).expand(expand_dims)
         for batch_w in batch_weights[::-1]:
             batch_w = batch_w.unsqueeze(-1)
-            d = len(batch_archetypes.shape) - len(batch_w.shape)
-            for _ in range(d):
+            empty_dims = len(batch_archetypes.shape) - len(batch_w.shape)
+            for _ in range(empty_dims):
                 batch_w = batch_w.unsqueeze(1)
             batch_archetypes = torch.matmul(batch_archetypes, batch_w).squeeze(-1)
         return batch_archetypes
 
-    def _cycle_dims(self, tensor, n):
+    def _cycle_dims(self, tensor, n_steps):
         """
         Cycle tensor dimensions from front to back for n steps
         """
-        for _ in range(n):
+        for _ in range(n_steps):
             tensor = tensor.unsqueeze(0).transpose(0, -1).squeeze(-1)
         return tensor
 
@@ -77,7 +82,7 @@ class MLP(nn.Module):
         activation=nn.ReLU,
         link_fn=identity_link,
     ):
-        super(MLP, self).__init__()
+        super().__init__()
         if layers > 0:
             mlp_layers = [nn.Linear(input_dim, width), activation()]
             for _ in range(layers - 1):
@@ -88,8 +93,10 @@ class MLP(nn.Module):
         self.mlp = nn.Sequential(*mlp_layers)
         self.link_fn = link_fn
 
-    def forward(self, x):
-        ret = self.mlp(x)
+    def forward(self, X):
+        """ Torch Forward pass.
+        """
+        ret = self.mlp(X)
         return self.link_fn(ret)
 
 
@@ -107,7 +114,7 @@ class NGAM(nn.Module):
         activation=nn.ReLU,
         link_fn=identity_link,
     ):
-        super(NGAM, self).__init__()
+        super().__init__()
         self.intput_dim = input_dim
         self.output_dim = output_dim
         self.nams = nn.ModuleList(
@@ -125,10 +132,12 @@ class NGAM(nn.Module):
         )
         self.link_fn = link_fn
 
-    def forward(self, x):
-        ret = self.nams[0](x[:, 0].unsqueeze(-1))
+    def forward(self, X):
+        """ Torch Forward pass.
+        """
+        ret = self.nams[0](X[:, 0].unsqueeze(-1))
         for i, nam in enumerate(self.nams[1:]):
-            ret += nam(x[:, i].unsqueeze(-1))
+            ret += nam(X[:, i].unsqueeze(-1))
         return self.link_fn(ret)
 
 
@@ -139,45 +148,45 @@ ENCODERS = {
 
 
 if __name__ == "__main__":
-    n = 100
-    x_dim = 10
-    y_dim = 5
-    k = 3
-    width = 50
-    layers = 5
-    x = torch.rand((n, x_dim))
+    N_SAMPLES = 100
+    X_DIM = 10
+    Y_DIM = 5
+    K_ARCHETYPES = 3
+    WIDTH = 50
+    LAYERS = 5
+    X_data = torch.rand((N_SAMPLES, X_DIM))
 
-    mlp = MLP(x_dim, y_dim, width, layers)
-    mlp(x)
+    mlp = MLP(X_DIM, Y_DIM, WIDTH, LAYERS)
+    mlp(X_data)
 
-    ngam = NGAM(x_dim, y_dim, width, layers)
-    ngam(x)
+    ngam = NGAM(X_DIM, Y_DIM, WIDTH, LAYERS)
+    ngam(X_data)
 
-    in_dims = (3, 4)
-    out_shape = (5, 6)
-    z1 = torch.randn(100, 3)
-    z2 = torch.randn(100, 4)
-    softselect = SoftSelect(in_dims, out_shape)
-    softselect(z1, z2)
+    IN_DIMS = (3, 4)
+    OUT_SHAPE = (5, 6)
+    Z1 = torch.randn(N_SAMPLES, IN_DIMS[0])
+    Z2 = torch.randn(N_SAMPLES, IN_DIMS[1])
+    softselect = SoftSelect(IN_DIMS, OUT_SHAPE)
+    softselect(Z1, Z2)
 
     precycle_vals = softselect.archetypes
-    assert precycle_vals.shape == (*out_shape, *in_dims)
+    assert precycle_vals.shape == (*OUT_SHAPE, *IN_DIMS)
     postcycle_vals = softselect.get_archetypes()
-    assert postcycle_vals.shape == (*in_dims, *out_shape)
-    softselect.set_archetypes(torch.randn(*in_dims, *out_shape))
+    assert postcycle_vals.shape == (*IN_DIMS, *OUT_SHAPE)
+    softselect.set_archetypes(torch.randn(*IN_DIMS, *OUT_SHAPE))
     assert (softselect.archetypes != precycle_vals).any()
     softselect.set_archetypes(postcycle_vals)
     assert (softselect.archetypes == precycle_vals).all()
 
-    in_dims = (3,)
-    explainer = Explainer(in_dims[0], out_shape)
-    explainer(z1)
+    IN_DIMS = (3,)
+    explainer = Explainer(IN_DIMS[0], OUT_SHAPE)
+    explainer(Z1)
 
     precycle_vals = explainer.archetypes
-    assert precycle_vals.shape == (*out_shape, *in_dims)
+    assert precycle_vals.shape == (*OUT_SHAPE, *IN_DIMS)
     postcycle_vals = explainer.get_archetypes()
-    assert postcycle_vals.shape == (*in_dims, *out_shape)
-    explainer.set_archetypes(torch.randn(*in_dims, *out_shape))
+    assert postcycle_vals.shape == (*IN_DIMS, *OUT_SHAPE)
+    explainer.set_archetypes(torch.randn(*IN_DIMS, *OUT_SHAPE))
     assert (explainer.archetypes != precycle_vals).any()
     explainer.set_archetypes(postcycle_vals)
     assert (explainer.archetypes == precycle_vals).all()
