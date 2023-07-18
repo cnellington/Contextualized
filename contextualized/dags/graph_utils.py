5a27e9627e9b49c8d51a5995a3c1c20c562c86d0
cnellington <caleb.n.ellington@gmail.com>
2023-07-17 22:59:17 -0700

    added faster thresholding method, although distance results are surprising

--- contextualized/dags/graph_utils.py
+++ contextualized/dags/graph_utils.py
@@ -139,56 +139,39 @@ def break_symmetry(w):
     return w
 
 
-# w is the weighted adjacency matrix
-def project_to_dag_search(w):
-    if is_dag(w):
-        return w, 0.0
-
-    w_dag = w.copy()
-    w_dag = break_symmetry(w_dag)
-
-    vals = sorted(list(set(np.abs(w_dag).flatten())))
+def project_to_dag_search(W):
+    W = W.copy()
+    if ig.Graph.Weighted_Adjacency(W).is_dag():
+        return W
+    W_mag = np.abs(W)
+    W_flat = W_mag.flatten()
+    
+    # Binary search for the minimum threshold where W is a DAG, O(|E|log|E|)
+    weights = np.sort(W_flat)
     low = 0
-    high = len(vals) - 1
-
-    def binary_search(arr, low, high, w):  # low and high are indices
-        # Check base case
-        if high == low:
-            return high
-        if high > low:
-            mid = (high + low) // 2
-            if mid == 0:
-                return mid
-            result = trim_params(w, arr[mid])
-            if is_dag(result):
-                result2 = trim_params(w, arr[mid - 1])
-                if is_dag(result2):  # middle value is too high.  go lower.
-                    return binary_search(arr, low, mid - 1, w)
-                else:
-                    return mid  # found it
-            else:  # middle value is too low.  go higher.
-                return binary_search(arr, mid + 1, high, w)
+    mid = 0
+    high = len(weights) - 1
+    while low < high - 1:
+        new_mid = (low + high) // 2
+        mid = new_mid
+#         print(low, mid, high)
+        if ig.Graph.Weighted_Adjacency(W * (W_mag > weights[mid])).is_dag():
+            high = mid
         else:
-            # Element is not present in the array
-            print("this should be impossible")
-            return -1
-
-    idx = binary_search(vals, low, high, w_dag) + 1
-    thresh = vals[idx]
-    w_dag = trim_params(w_dag, thresh)
-
-    # Now add back in edges with weights smaller than the thresh that don't violate DAG-ness.
-    # want a list of edges (i, j) with weight in decreasing order.
-    all_vals = np.abs(w_dag).flatten()
-    idxs_sorted = reversed(np.argsort(all_vals))
-    for idx in idxs_sorted:
-        i = idx // w_dag.shape[1]
-        j = idx % w_dag.shape[1]
-        if np.abs(w[i][j]) > thresh:  # already retained
-            continue
-        w_dag[i][j] = w[i][j]
-        if not is_dag(w_dag):
-            w_dag[i][j] = 0.0
-
-    assert is_dag(w_dag)
-    return w_dag
+            low = mid
+    W_dag = W * (W_mag > weights[high])
+    
+    # Re-add edges we removed that don't violate the topological order, O(|E|)
+    p = len(W_dag)
+    weights_i = np.argsort(W_flat)
+    toposort = ig.Graph.Weighted_Adjacency(W_dag).topological_sorting()
+    toposort_lookup = np.zeros(p)
+    for topo_i, topo_node in enumerate(toposort):
+        toposort_lookup[topo_node] = topo_i
+    for sorted_i in range(high, -1, -1):
+        i = weights_i[sorted_i]
+        parent_i = i // p
+        child_i = i % p
+        if toposort_lookup[parent_i] < toposort_lookup[child_i]:
+            W_dag[parent_i, child_i] = weights[sorted_i]
+    return W_dag
