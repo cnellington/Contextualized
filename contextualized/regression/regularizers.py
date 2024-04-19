0bdcbab4e6491bf5eda96086038586ba4cddf3cb
cnellington <caleb.n.ellington@gmail.com>
2024-04-19 14:11:35 -0400

    remove lambdas from arguments, cannot be pickled, replaced with functools.partial. Added tests to modules and save/load utils

--- contextualized/regression/regularizers.py
+++ contextualized/regression/regularizers.py
@@ -2,13 +2,21 @@
 Torch regularizers used for regression.
 """
 import torch
+from functools import partial
 
+
+def no_reg_fn(beta, mu):
+    return 0.0
 
 def no_reg():
     """
     Function that returns an empty regularizer.
     """
-    return lambda beta, mu: 0.0
+    return no_reg_fn
+
+
+def l1_reg_fn(alpha, mu_ratio, beta, mu):
+    return alpha * (mu_ratio * torch.norm(mu, p=1) + (1 - mu_ratio) * torch.norm(beta, p=1)).mean()
 
 
 def l1_reg(alpha, mu_ratio=0.5):
@@ -18,12 +26,11 @@ def l1_reg(alpha, mu_ratio=0.5):
     :param mu_ratio:  (Default value = 0.5)
 
     """
-    return (
-        lambda beta, mu: alpha
-        * (
-            mu_ratio * torch.norm(mu, p=1) + (1 - mu_ratio) * torch.norm(beta, p=1)
-        ).mean()
-    )
+    return partial(l1_reg_fn, alpha, mu_ratio)
+
+
+def l2_reg_fn(alpha, mu_ratio, beta, mu):
+    return alpha * (mu_ratio * torch.norm(mu, p=2) + (1 - mu_ratio) * torch.norm(beta, p=2)).mean()
 
 
 def l2_reg(alpha, mu_ratio=0.5):
@@ -33,12 +40,15 @@ def l2_reg(alpha, mu_ratio=0.5):
     :param mu_ratio:  (Default value = 0.5)
 
     """
-    return (
-        lambda beta, mu: alpha
-        * (
-            mu_ratio * torch.norm(mu, p=2) + (1 - mu_ratio) * torch.norm(beta, p=2)
-        ).mean()
-    )
+    return partial(l2_reg_fn, alpha, mu_ratio)
+
+
+def l1_l2_reg_fn(alpha, l1_ratio, mu_ratio, beta, mu):
+    return alpha * (
+        l1_ratio * (mu_ratio * torch.norm(mu, p=1) + (1 - mu_ratio) * torch.norm(beta, p=1))
+        + (1 - l1_ratio) * (mu_ratio * torch.norm(mu, p=2) + (1 - mu_ratio) * torch.norm(beta, p=2)
+        )
+    ).mean()
 
 
 def l1_l2_reg(alpha, l1_ratio=0.5, mu_ratio=0.5):
@@ -49,15 +59,7 @@ def l1_l2_reg(alpha, l1_ratio=0.5, mu_ratio=0.5):
     :param mu_ratio:  (Default value = 0.5)
 
     """
-    return (
-        lambda beta, mu: alpha
-        * (
-            l1_ratio
-            * (mu_ratio * torch.norm(mu, p=1) + (1 - mu_ratio) * torch.norm(beta, p=1))
-            + (1 - l1_ratio)
-            * (mu_ratio * torch.norm(mu, p=2) + (1 - mu_ratio) * torch.norm(beta, p=2))
-        ).mean()
-    )
+    return partial(l1_l2_reg_fn, alpha, l1_ratio, mu_ratio)
 
 
 REGULARIZERS = {"none": no_reg(), "l1": l1_reg, "l2": l2_reg, "l1_l2": l1_l2_reg}
--- tests.py
+++ tests.py
@@ -1,11 +1,8 @@
 import unittest
+from contextualized.tests import *
 from contextualized.regression.tests import *
 from contextualized.dags.tests import *
-from contextualized.easy.tests.test_regressor import *
-from contextualized.easy.tests.test_classifier import *
-from contextualized.easy.tests.test_markov_networks import *
-from contextualized.easy.tests.test_correlation_networks import *
-from contextualized.easy.tests.test_bayesian_networks import *
+from contextualized.easy.tests import *
 from contextualized.analysis.tests import *
 from contextualized.baselines.tests import *
 
