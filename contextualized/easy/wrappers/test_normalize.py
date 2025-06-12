5fac4a6fe90fab521de5019e1d9912a49eadb630
Peter Li <peterli@Mac.lan>
2025-06-12 10:13:05 -0500

    re-commit for cleaning branch

---
+++ contextualized/easy/wrappers/test_normalize.py
@@ -0,0 +1,54 @@
+"""
+test for issue #250
+
+"""
+
+import os
+import sys
+import numpy as np
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from contextualized.easy import ContextualizedRegressor
+
+# generate random un-normalized C and X
+np.random.seed(42)
+C = np.random.rand(100, 5) * 10
+X = np.random.rand(100, 3) * 10
+Y = np.random.rand(100, 1)
+
+# split training/testing data
+C_train, C_test = C[:80], C[80:]
+X_train, X_test = X[:80], X[80:]
+Y_train, Y_test = Y[:80], Y[80:]
+
+# initialized with normalize=True
+model = ContextualizedRegressor(normalize=True)
+
+# train model on training data
+model.fit(C_train, X_train, Y_train)
+
+assert np.allclose(
+    model.scaler_C.mean_, np.mean(C_train, axis=0), atol=1e-1
+), "C normalization failed!"
+assert np.allclose(
+    model.scaler_C.scale_, np.std(C_train, axis=0), atol=1e-1
+), "C normalization failed!"
+assert np.allclose(
+    model.scaler_X.mean_, np.mean(X_train, axis=0), atol=1e-1
+), "X normalization failed!"
+assert np.allclose(
+    model.scaler_X.scale_, np.std(X_train, axis=0), atol=1e-1
+), "X normalization failed!"
+print("\n✅ fit() successfully normalized C_train and X_train!")
+# predict on test data by trained model
+Y_pred = model.predict(C_test, X_test)
+
+assert Y_pred is not None, "predict() failed!"
+assert Y_pred.shape == Y_test.shape, "predict() has wrong output shape!"
+
+print(
+    "\n✅ predict() ran successfully, un-normalized C_test and X_test were correctly treated!"
+)
+
+print("predicted value:", Y_pred)
--- contextualized/utils.py
+++ contextualized/utils.py
@@ -62,3 +62,40 @@ class DummyYPredictor:
         """
         n = len(args[0])
         return torch.zeros((n, *self.y_dim))
+
+
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+
+def normalize_data(C, X, return_scaler=False):
+    """
+    Normalize C and X
+    """
+    scaler_C = StandardScaler()
+    scaler_X = StandardScaler()
+
+    C_norm = scaler_C.fit_transform(C)
+    X_norm = scaler_X.fit_transform(X)
+
+    if isinstance(C, pd.DataFrame):
+        C_norm = pd.DataFrame(C_norm, columns=C.columns, index=C.index)
+    if isinstance(X, pd.DataFrame):
+        X_norm = pd.DataFrame(X_norm, columns=X.columns, index=X.index)
+
+    if return_scaler:
+        return C_norm, X_norm, scaler_C, scaler_X
+    return C_norm, X_norm
+
+
+def inverse_transform_preserve_df(scaler, df, reference=None):
+    X_inv = scaler.inverse_transform(df)
+
+    if isinstance(df, pd.DataFrame):
+        return pd.DataFrame(X_inv, columns=df.columns, index=df.index)
+
+    if isinstance(reference, pd.DataFrame):
+        return pd.DataFrame(X_inv, columns=reference.columns, index=reference.index)
+
+    return X_inv
