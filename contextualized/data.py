5d5c08211a2f580dac1fdccf67572081bc583d00
cnellington <caleb.n.ellington@gmail.com>
2025-07-13 10:08:10 -0700

    Make datamodules useful

--- contextualized/data.py
+++ contextualized/data.py
@@ -1,21 +1,46 @@
 import torch
 from lightning import LightningDataModule
 from contextualized.regression.datasets import MultivariateDataset, UnivariateDataset
+from sklearn.model_selection import train_test_split
 
-class MyRegressionDataModule(LightningDataModule):
-    def __init__(self, batch_size: int = 32):
+class RegressionDataModule(LightningDataModule):
+    def __init__(
+            self, 
+            C_train, 
+            X_train, 
+            Y_train,
+            C_val,
+            X_val,
+            Y_val,
+            C_test,
+            X_test,
+            Y_test,
+            C_predict,
+            X_predict,
+            Y_predict,
+            batch_size: int = 32
+    ):
         super().__init__()
         self.batch_size = batch_size
-        self.C = torch.randn(100, (10))
-        self.X = torch.randn(100, (10))
-        self.Y = torch.randn(100, (1))
+        self.C_train = C_train
+        self.X_train = X_train
+        self.Y_train = Y_train
+        self.C_val = C_val
+        self.X_val = X_val
+        self.Y_val = Y_val
+        self.C_test = C_test
+        self.X_test = X_test
+        self.Y_test = Y_test
+        self.C_predict = C_predict
+        self.X_predict = X_predict
+        self.Y_predict = Y_predict
 
     def setup(self, stage=None):
         # Called on every GPU
-        self.train_dataset = MultivariateDataset(self.C, self.X, self.Y)
-        self.val_dataset = MultivariateDataset(self.C, self.X, self.Y)
-        self.test_dataset = MultivariateDataset(self.C, self.X, self.Y)
-        self.predict_dataset = MultivariateDataset(self.C, self.X, self.Y)
+        self.train_dataset = MultivariateDataset(self.C_train, self.X_train, self.Y_train)
+        self.val_dataset = MultivariateDataset(self.C_val, self.X_val, self.Y_val)
+        self.test_dataset = MultivariateDataset(self.C_test, self.X_test, self.Y_test)
+        self.predict_dataset = MultivariateDataset(self.C_predict, self.X_predict, self.Y_predict)
 
     def train_dataloader(self):
         # Called on every GPU
@@ -34,19 +59,36 @@ class MyRegressionDataModule(LightningDataModule):
         return torch.utils.data.DataLoader(self.predict_dataset, batch_size=self.batch_size, shuffle=False)
     
 
-class MyCorrelationDataModule(LightningDataModule):
-    def __init__(self, batch_size: int = 32):
+class CorrelationDataModule(LightningDataModule):
+    def __init__(
+        self,
+        C_train,
+        X_train,
+        C_val,
+        X_val,
+        C_test,
+        X_test,
+        C_predict,
+        X_predict,
+        batch_size: int = 32
+    ):
         super().__init__()
         self.batch_size = batch_size
-        self.C = torch.randn(100, (10))
-        self.X = torch.randn(100, (3))
+        self.C_train = C_train
+        self.X_train = X_train
+        self.C_val = C_val
+        self.X_val = X_val
+        self.C_test = C_test
+        self.X_test = X_test
+        self.C_predict = C_predict
+        self.X_predict = X_predict
 
     def setup(self, stage=None):
         # Called on every GPU
-        self.train_dataset = UnivariateDataset(self.C, self.X, self.X)
-        self.val_dataset = UnivariateDataset(self.C, self.X, self.X)
-        self.test_dataset = UnivariateDataset(self.C, self.X, self.X)
-        self.predict_dataset = UnivariateDataset(self.C, self.X, self.X)
+        self.train_dataset = UnivariateDataset(self.C_train, self.X_train, self.X_train)
+        self.val_dataset = UnivariateDataset(self.C_val, self.X_val, self.X_val)
+        self.test_dataset = UnivariateDataset(self.C_test, self.X_test, self.X_test)
+        self.predict_dataset = UnivariateDataset(self.C_predict, self.X_predict, self.X_predict)
 
     def train_dataloader(self):
         return torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
